5e8fd51a0a6bc30251ddaf46ed1ab4ed4ffb85bf
biel-365 <aroiogabriel@gmail.com>
2025-08-08 16:35:13 -0300

    Terminando tudo

---
+++ AppTarefas/Controllers/TarefasController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppTarefas.Controllers
+{
+    public class TarefasController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AppTarefas/Models/Tarefa.cs
@@ -0,0 +1,10 @@
+﻿namespace AppTarefas.Models
+{
+    public class Tarefa
+    {
+        public int TarefaId { get; set; } // Nome da chave Primária tem que ter Id (id é a chave primária)
+        public string? Titulo { get; set; }
+        public string? Descricao { get; set; }
+        public bool Concluida { get; set; }
+    }
+}
